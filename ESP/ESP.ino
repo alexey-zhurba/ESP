eac1dc33657e68e36914044a304d90b506b4fca4
alexe <alexey.zhurba.az@gmail.com>
2022-10-31 19:39:42 +0100

    Add project files.

---
+++ ESP/ESP.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ESP.ino
+ Created:	10/31/2022 7:38:07 PM
+ Author:	alexe
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
