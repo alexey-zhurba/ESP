65ca9923d1e5a2676e8954da544b6dcc43726979
alexey-zhurba <38475937+alexey-zhurba@users.noreply.github.com>
2022-11-01 15:22:44 +0100

    - include RoboApi

--- ESP/ESP.ino
+++ ESP/ESP.ino
@@ -4,9 +4,19 @@
  Author:	alexe
 */
 
+#include <Timer.h>
+#include <TimedObjectManager.h>
+#include <TimedObject.h>
+#include <MotorGroup.h>
+#include <MotorController.h>
+#include <IRSensor.h>
+#include <Debug.h>
+#include <Arrays.h>
+#include "src/RoboApi.h"
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	
 }
 
 // the loop function runs over and over again until power down or reset
