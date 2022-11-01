f752c61d3a7951ea65f09cf07156119db8608dee
alexey-zhurba <38475937+alexey-zhurba@users.noreply.github.com>
2022-11-01 15:27:03 +0100

    - ESP.ino

--- ESP/ESP.ino
+++ ESP/ESP.ino
@@ -4,16 +4,17 @@
  Author:	alexe
 */
 
-#include <Timer.h>
-#include <TimedObjectManager.h>
-#include <TimedObject.h>
-#include <MotorGroup.h>
-#include <MotorController.h>
-#include <IRSensor.h>
-#include <Debug.h>
-#include <Arrays.h>
-#include "src/RoboApi.h"
+#include <RoboApi.h>
 
+#define IR_SENS_L 1
+#define IR_SENS_R 2
+#define MOTOR_L_IA 3
+#define MOTOR_L_IB 2
+#define MOTOR_R_IA 19
+#define MOTOR_R_IB 18
+
+RoboApi::IRSensor irSensLinks(IR_SENS_L), irSensRechts(IR_SENS_R);
+RoboApi::MotorGroup motor(RoboApi::MotorController(MOTOR_L_IA, MOTOR_L_IB), RoboApi::MotorController(MOTOR_R_IA, MOTOR_R_IB));
 // the setup function runs once when you press reset or power the board
 void setup() {
 	
@@ -21,5 +22,5 @@ void setup() {
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	RoboApi::TimedObjectManager::instance()->update();
 }
